7fa1dc0765ab333a7d0df042d65719a4f4e07b33
Spirit ser <96570722+SPlRlT-YT@users.noreply.github.com>
2022-01-07 22:15:03 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,18 +7,18 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/aAGjt7z.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/P47w0P3.jpeg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*spirit  bot created by Spirit*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Spirit ser bot created by Spirit *
 *Creator number : wa.me/18052196558?text=Hi%20spirit%20.%20*
 
 *spirit bot group* : https://chat.whatsapp.com/FLqVrc4RdakIjXqcNYz7vU
 
-*Githublink (Setup)* :    https://github.com/SPlRlT-YT/spirit-bot
+*Githublink (Setup)* :    https://github.com/SPlRlT-YT/Spirit-ser
 
-*Audio commads* :   https://github.com/SPlRlT-YT/spirit-bot/tree/master/uploads
+*Audio commads* :   https://github.com/SPlRlT-YT/Spirit-ser/tree/master/uploads
 
-*Sticker commads* : https://github.com/SPlRlT-YT/spirit-bot/tree/master/sticker
+*Sticker commads* : https://github.com/SPlRlT-YT/Spirit-ser/tree/master/sticker
 
 *My channel* : https://youtube.com/channel/UCBv1Z1zkOmdGeWU2QRdYBnA 
 
@@ -26,4 +26,3 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 `}) 
 
 }));
-
