68a00a92f8c280d9d149181f2303af852dcdf966
Spirit ser <96570722+SPlRlT-YT@users.noreply.github.com>
2022-01-07 23:34:05 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,22 +7,13 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/P47w0P3.jpeg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://telegra.ph/file/5c4082bd35a3866b47978.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Spirit ser bot created by Spirit *
-*Creator number : wa.me/18052196558?text=Hi%20spirit%20.%20*
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*RAJU-BHAI Bot Created By ABHIRAM SACHU AND SARATH ACHU*
+This a Private bot 
+*PUBLIC BOT (LINK)  :    https://github.com/DEVIL-SARATH/RajuBhai*
+ PUBLIC BOT 2 (LINK)   :  https://github.com/KINGS-AS/RDX
 
-*spirit bot group* : https://chat.whatsapp.com/FLqVrc4RdakIjXqcNYz7vU
-
-*Githublink (Setup)* :  https://github.com/SPlRlT-YT/Spirit-ser
-
-*Audio commads* :   https://github.com/SPlRlT-YT/Spirit-ser/tree/master/uploads
-
-*Sticker commads* : https://github.com/SPlRlT-YT/Spirit-ser/tree/master/sticker
-
-*My channel* : https://youtube.com/channel/UCBv1Z1zkOmdGeWU2QRdYBnA 
-
-*For More Updates Subscribe The Channel☝*
 `}) 
 
 }));
