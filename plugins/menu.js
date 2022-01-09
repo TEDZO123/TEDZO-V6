1e190f077fb621b71b2e8accfec7540061477731
Spirit ser <96570722+SPlRlT-YT@users.noreply.github.com>
2022-01-09 18:41:45 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -17,7 +17,7 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━♥sᴘɪʀɪᴛ ʙᴏᴛ♥━━━━━⛦
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━💝sᴘɪʀɪᴛ ʙᴏᴛ💝━━━━━⛦
 
 
 𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 sᴘɪʀɪᴛ 𝐛𝐨𝐭.
