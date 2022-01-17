e1807591b189a7e295f857899667cae80c731f5e
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-01-17 17:43:24 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -7,20 +7,25 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/aO8uCtQ.jpg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/owr7tXU.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*aslusir created this bot*
 
-🤖 *Creator number : wa.me/917736835721?text=Hi%20tedzo%20%20*
+🤖 *Creator number*
+wa.me/917736835721?text=Hi%20tedzo%20%20*
 🤖 
-🤖 *Git hub        : https://github.com/poodqpotta*
+🤖 *Git hub*  
+https://github.com/poodqpotta*
 🤖
-🤖 *WHATSAPP GROUP : https://chat.whatsapp.com/JqUKjz9djTS9mDmxA2ph2t
+🤖 *WHATSAPP GROUP*
+https://chat.whatsapp.com/JqUKjz9djTS9mDmxA2ph2t
 🤖
-🤖 *Instagram      : https://www.instagram.com/a_s_l_a_m_._x*
+🤖 *Instagram*    
+https://www.instagram.com/a_s_l_a_m_._x*
 🤖
-🤖 *WEBSITE        : Available soon....
-🤖
+🤖 *WEBSITE*      
+Available soon....
+
 `}) 
 
 }));
