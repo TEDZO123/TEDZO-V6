4f5060abbfbb8015d617509667e69c55bce6dcc4
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-01-28 17:48:45 +0530

    Update tedzo1.js

--- plugins/tedzo1.js
+++ plugins/tedzo1.js
@@ -1,9 +1,4 @@
-/* Copyright (C) 2021 AMALSER.   
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-Kaztroser - Ajfx 
-Wa.me/+918281440156
-*/
+/*asluser999@gmail.com*/
 
 const Neotro = require('../events');
 const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
@@ -38,6 +33,6 @@ Neotro.addCommand({pattern: 'menu', fromMe: false, dontAddCommandList: true}, (a
         listType: 1
        }
        
-       await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage, { mimetype: Mimetype.buttonsMessage, quoted: message.data, ptt: true,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 0000000, status: 200, thumbnail: fs.readFileSync('./photo/tedzo.png'), surface: 200, message: Config.BOT, orderTitle: Config.BOT, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOT + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./photo/tedzo.png')}}}});
-
-}));
+       await message.client.sendMessage(message.jid, button, MessageType.listMessage)
+    
+    }));
