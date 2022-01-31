60e8a95ba03fc2e7fb952bf3673311092abfab83
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-01-31 12:18:26 +0530

    Update pm block.js

--- plugins/pm block.js
+++ plugins/pm block.js
@@ -40,7 +40,7 @@ if (Config.LANG == 'EN') plk = 'sorry chatting in pm is not allowed'
 if (Config.LANG == 'EN') plk2 = '.block'
 if (Config.LANG == 'ML') plk = 'ക്ഷമിക്കണം PM ൽ ചാറ്റിംഗ് അനുവദനീയമല്ല'
 if (Config.LANG == 'ML') plk2 = '.block'
-asena.addCommand({on: 'text', fromMe: false, onlyPm: true , deleteCommand: false}, (async (message, match) => {
+Asena.addCommand({on: 'text', fromMe: false, onlyPm: true , deleteCommand: false}, (async (message, match) => {
     if (pmblock_var == 'true' && message.jid !== '917736835721@s.whatsapp.net') {
         let regex1 = anything
         if (!regex1.test(message.message)) {
