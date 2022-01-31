add0d009754f832785749f7ad0692a5764f14273
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-01-31 10:06:56 +0530

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,6 +1,6 @@
 /* Copyright (C) 2020 Yusuf Usta.
-recodded by As
-new work type by As
+recodded by afnanplk
+new work type by afnanplk
 */
 
 const Asena = require('../events');
@@ -90,7 +90,7 @@ else if (Config.WORKTYPE == 'public') {
             mesaj = '';
             grup['participants'].map(
                 async (uye) => {
-                    mesaj += '\n 👤 @' + uye.id.split('@')[0] + 'ㅤ';
+                    mesaj += '\n 🦋 @' + uye.id.split('@')[0] + 'ㅤ🍁';
                     jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
                 }
             );
@@ -155,7 +155,7 @@ async function checkImAdmin(message, user = message.client.user.jid) {
             mesaj = '';
             grup['participants'].map(
                 async (uye) => {
-                    mesaj += '\n 👤 @' + uye.id.split('@')[0] + 'ㅤ';
+                    mesaj += '\n 🦋 @' + uye.id.split('@')[0] + 'ㅤ🍁';
                     jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
                 }
             );
